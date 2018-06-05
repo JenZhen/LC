c1a94b3cbac699cefa345f8e848aadf3939cc859
Jen Zhen <jenzhen.nyc89@yahoo.com>
2018-06-05 08:24:33 -0400

    add more for bs

--- lc_ladder/advanced/binary-search/Find_The_Duplicate_Number.py
+++ lc_ladder/advanced/binary-search/Find_The_Duplicate_Number.py
@@ -11,14 +11,53 @@
 # Example
 
 """
-Algo:
+Algo: Binary Search
 D.S.:
 
 Solution:
+Similar to: Wood_Cut, Copy_Books
 
+1. 找到可行解的范围: 1 - n (只有一个数重复，数的可能范围1 - n, 共 n + 1个数)
+2. 猜答案 （二分）: (l + r ) // 2，对可行解范围进行二分
+3. 检验条件，来判断答案应该在哪一侧
+getCntLT: 遍历数组，数<= target的数有几个。例，target=4, 数到<=4的有<=4个，说明重复的数在4右边，比4大
+4. 调整搜索范围
+特征：# TODO 不明显
+
+Time：O(nlogn)
 
 Corner cases:
 """
+
+class Solution:
+    """
+    @param nums: an array containing n + 1 integers which is between 1 and n
+    @return: the duplicate one
+    """
+    def findDuplicate(self, nums):
+        # write your code here
+        if not nums:
+            return 0
+        l, r = 1, len(nums)
+        while l + 1 < r:
+            mid = (l + r) // 2
+            if self.getCntLT(nums, mid) > mid:
+                # dup is less than mid
+                r = mid
+            else:
+                l = mid
+        # 说明比l小的数里面没有重复的，在l的右边
+        if self.getCntLT(nums, l) <= l:
+            return r
+        else:
+            return l
+
+    def getCntLT(self, nums, target):
+        res = 0
+        for num in nums:
+            if num <= target:
+                res += 1
+        return res
 
 # Test Cases
 if __name__ == "__main__":
--- lc_ladder/advanced/binary-search/Maximum_Average_Subarray.py
+++ lc_ladder/advanced/binary-search/Maximum_Average_Subarray.py
@@ -1,17 +1,33 @@
 #! /usr/local/bin/python3
 
-# Requirement
+# https://www.lintcode.com/problem/maximum-average-subarray-i/description
 # Example
 
 """
-Algo:
-D.S.:
+Algo: Similar to Maximum_Average_Subarray II but not binary search required
+D.S.: Sliding Window
 
 Solution:
-
-
+Time O(n)
 Corner cases:
 """
+
+class Solution:
+    """
+    @param nums: an array
+    @param k: an integer
+    @return: the maximum average value
+    """
+    def findMaxAverage(self, nums, k):
+        # Write your code here
+        if not nums or not k:
+            return 0
+        ttl = sum(nums[:k])
+        res = ttl
+        for i in range(k, len(nums)):
+            ttl += (nums[i] - nums[i - k])
+            res = max(res, ttl)
+        return res / k
 
 # Test Cases
 if __name__ == "__main__":
