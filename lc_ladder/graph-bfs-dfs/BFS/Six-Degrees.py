5c57b84298594ee7ee232263714a55b32461a7b8
Jen Zhen <jenzhen.nyc89@yahoo.com>
2018-06-25 21:19:36 -0400

    Six-Degrees BFS

--- lc_ladder/graph-bfs-dfs/BFS/Six-Degrees.py
+++ lc_ladder/graph-bfs-dfs/BFS/Six-Degrees.py
@@ -4,14 +4,52 @@
 # Example
 
 """
-Algo:
-D.S.:
+Algo: BFS, DFS
+D.S.: UndirectedGraphNode
 
 Solution:
-
+1. BFS
+Use dict = {} key = node, value = dist (Starting from 0) to
+2. DFS
 
 Corner cases:
 """
+
+# Definition for Undirected graph node
+# class UndirectedGraphNode:
+#     def __init__(self, x):
+#         self.label = x
+#         self.neighbors = []
+
+
+from collections import deque
+class Solution1:
+    '''
+    @param {UndirectedGraphNode[]} graph a list of Undirected graph node
+    @param {UndirectedGraphNode} s, t two Undirected graph nodes
+    @return {int} an integer
+    '''
+    def sixDegrees(self, graph, s, t):
+        if not graph or not s or not t:
+            return -1 # invalid result not 0 distant
+
+        dist = {}
+        visited = {}
+        q = deque([])
+
+        q.append(s)
+        dist[s] = 0
+
+        while len(q):
+            curNode = q.leftpop()
+            if curNode == t:
+                return dist[curNode]
+            for nei in curNode.neighbors:
+                if not visited[nei]:
+                    q.append(nei)
+                    dist[nei] = dist[curNode] + 1
+                    visited[nei] = True
+        return -1 # t not found
 
 # Test Cases
 if __name__ == "__main__":
