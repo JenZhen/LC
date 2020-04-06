f2855c946365ba5c4e10c40b100af3c915bbc10a
Jen Zhen <jenzhen.nyc89@yahoo.com>
2020-04-06 09:38:25 -0400

    add notes for two dp

--- lc_ladder/Basic_Algo/dp/Climbing_Stairs_II.py
+++ lc_ladder/Basic_Algo/dp/Climbing_Stairs_II.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
 # http://www.jiuzhang.com/solution/climbing-stairs-ii/
-# A child is running up a staircase with n steps, and can hop either 1 step, 2 steps, or 3 steps at a time. Implement a method to count how many possible ways the child can run up the stairs.
+# A child is running up a staircase with n steps, and can hop either 1 step, 2 steps, or 3 steps at a time.
+# Implement a method to count how many possible ways the child can run up the stairs.
 # Example
 
 """
