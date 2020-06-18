6497d890f2755925150e3b19266542dc9dac2088
Jen Zhen <jenzhen.nyc89@yahoo.com>
2020-06-18 09:07:42 -0400

    add a question

--- lc_ladder/company/fb/Trapping_Rain_Water.py
+++ lc_ladder/company/fb/Trapping_Rain_Water.py
@@ -14,6 +14,35 @@ Time: O(1)
 
 Corner cases:
 """
+class Solution:
+    """
+    @param heights: a list of integers
+    @return: a integer
+    """
+    def trapRainWater(self, heights):
+        # write your code here
+        if not heights:
+            return 0
+        l, r = 0, len(heights) - 1
+        lHeight = heights[l]
+        rHeight = heights[r]
+        res = 0
+        while l < r:
+            if lHeight < rHeight:
+                # search left side
+                l += 1
+                if lHeight > heights[l]:
+                    res += (lHeight - heights[l])
+                else:
+                    lHeight = heights[l]
+            else:
+                # search right side
+                r -= 1
+                if rHeight > heights[r]:
+                    res += (rHeight - heights[r])
+                else:
+                    rHeight = heights[r]
+        return res
 
 # Test Cases
 if __name__ == "__main__":
